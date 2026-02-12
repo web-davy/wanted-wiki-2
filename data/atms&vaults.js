d00f35521e4cf68cd2d7eba2de73039c06ef2907
DevvGames <102315175+DevvGamesRblx@users.noreply.github.com>
2026-02-12 14:57:10 +0200

    Update atms&vaults.js

--- data/atms&vaults.js
+++ data/atms&vaults.js
@@ -1,19 +1,19 @@
 const ATMS_DATA = [
-  { name: "Green ATM", rarity: "COMMON", price: 200 },
-  { name: "Blue ATM", rarity: "COMMON", price: 300 },
-  { name: "Purple ATM", rarity: "UNCOMMON", price: 500 },
-  { name: "Red ATM", rarity: "EPIC", price: 750 },
-  { name: "Gold ATM", rarity: "MYTHIC", price: 10000 },
-  { name: "Diamond ATM", rarity: "LEGENDARY", price: 100000 },
-  { name: "Void ATM", rarity: "LEGENDARY", price: 500000 }
+  { name: "Green ATM", rarity: "COMMON", rarityPercent: "?%", price: 200 },
+  { name: "Blue ATM", rarity: "COMMON", rarityPercent: "?%", price: 300 },
+  { name: "Purple ATM", rarity: "UNCOMMON", rarityPercent: "?%", price: 500 },
+  { name: "Red ATM", rarity: "EPIC", rarityPercent: "?%", price: 750 },
+  { name: "Gold ATM", rarity: "MYTHIC", rarityPercent: "?%", price: 10000 },
+  { name: "Diamond ATM", rarity: "LEGENDARY", rarityPercent: "?%", price: 100000 },
+  { name: "Void ATM", rarity: "LEGENDARY", rarityPercent: "?%", price: 500000 }
 ];
 
 const VAULTS_DATA = [
-  { name: "Regular Vault", rarity: "COMMON", price: 500 },
-  { name: "Silver Vault", rarity: "COMMON", price: 1000 },
-  { name: "Gold Vault", rarity: "UNCOMMON", price: 2500 },
-  { name: "Sapphire Vault", rarity: "EPIC", price: 5000 },
-  { name: "Ruby Vault", rarity: "LEGENDARY", price: 15000 },
-  { name: "Emerald Vault", rarity: "LEGENDARY", price: 50000 },
-  { name: "Amethyst Vault", rarity: "MYTHIC", price: 100000 }
+  { name: "Regular Vault", rarity: "COMMON", rarityPercent: "~53.5%", price: 500 },
+  { name: "Silver Vault", rarity: "COMMON", rarityPercent: "~26.5%", price: 1000 },
+  { name: "Gold Vault", rarity: "UNCOMMON", rarityPercent: "~17%", price: 2500 },
+  { name: "Sapphire Vault", rarity: "EPIC", rarityPercent: "~2.2%", price: 5000 },
+  { name: "Ruby Vault", rarity: "LEGENDARY", rarityPercent: "~0.45%", price: 15000 },
+  { name: "Emerald Vault", rarity: "LEGENDARY", rarityPercent: "~0.25%", price: 50000 },
+  { name: "Amethyst Vault", rarity: "MYTHIC", rarityPercent: "~0.05%", price: 100000 }
 ];
